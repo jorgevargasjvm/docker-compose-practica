5226ef8b101dffea87b6c53f7a509ec4110bcad4
Jorge Vargas <jorgevargasmateo@hotmail.com>
2020-10-26 09:19:54 -0400

    initial commit

---
+++ src/main/java/com/vargas/DockerComposePractica/DockerComposePracticaApplication.java
@@ -0,0 +1,13 @@
+package com.vargas.DockerComposePractica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerComposePracticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerComposePracticaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vargas/DockerComposePractica/DockerComposePracticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vargas.DockerComposePractica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerComposePracticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
